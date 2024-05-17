91f952976a7cdd18c31631e8ef5802d9508e666d
kiseokkm <311kskim@gmail.com>
2024-05-17 09:46:56 +0900

    Feat : Initial commit

---
+++ src/main/java/com/sparta/nbcamp_java_5th_schedulemanagement/NbcampJava5thScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_schedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcampJava5thScheduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcampJava5thScheduleManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/nbcamp_java_5th_schedulemanagement/NbcampJava5thScheduleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.nbcamp_java_5th_schedulemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcampJava5thScheduleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
