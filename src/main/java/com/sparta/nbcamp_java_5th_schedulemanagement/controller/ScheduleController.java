8065478ac5401e78aa14e8975aa5007261d1b18f
kiseokkm <311kskim@gmail.com>
2024-05-20 16:57:24 +0900

    🩹 fix : [feedback 1] ScheduleController API 엔드포인트 수정

--- src/main/java/com/sparta/nbcamp_java_5th_schedulemanagement/controller/ScheduleController.java
+++ src/main/java/com/sparta/nbcamp_java_5th_schedulemanagement/controller/ScheduleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/schedules")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
@@ -16,27 +16,27 @@ public class ScheduleController {
         this.scheduleService = scheduleService;
     }
 
-    @PostMapping("/schedules")
+    @PostMapping
     public ScheduleResponseDto createSchedule(@RequestBody ScheduleRequestDto scheduleRequestDto) {
         return scheduleService.createSchedule(scheduleRequestDto);
     }
 
-    @GetMapping("/schedules")
-    public List<ScheduleResponseDto> getSchedules() {
+    @GetMapping
+    public List<ScheduleResponseDto> getAllSchedules() {
         return scheduleService.getAllSchedules();
     }
 
-    @GetMapping("/schedules/{id}")
+    @GetMapping("/{id}")
     public ScheduleResponseDto getSchedule(@PathVariable Long id) {
         return scheduleService.getSchedule(id);
     }
 
-    @PutMapping("/schedules/{id}/{password}")
+    @PutMapping("/{id}/{password}")
     public Long updateSchedule(@PathVariable Long id, @PathVariable String password, @RequestBody ScheduleRequestDto scheduleRequestDto) {
         return scheduleService.updateSchedule(id, password, scheduleRequestDto);
     }
 
-    @DeleteMapping("/schedules/{id}/{password}")
+    @DeleteMapping("/{id}/{password}")
     public Long deleteSchedule(@PathVariable Long id, @PathVariable String password) {
         return scheduleService.deleteSchedule(id, password);
     }
